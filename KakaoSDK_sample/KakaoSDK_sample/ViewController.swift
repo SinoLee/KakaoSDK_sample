96098a6b525f552e9c2efcfc5fe4790585d985f4
sinoLee <sino.lty@gmail.com>
2019-06-12 23:45:50 +0900

    Change "SwiftUI" -> "Swift"

---
+++ KakaoSDK_sample/KakaoSDK_sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KakaoSDK_sample
+//
+//  Created by Lee Taeyoun on 2019/06/12.
+//  Copyright © 2019 Sino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
